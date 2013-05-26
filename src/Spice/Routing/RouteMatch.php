475f701bf81d19d06c17db4a9922cd8bc37685f0
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-05-26 17:09:35 +0200

    Iniciando mecanismo de roteamento de requisições

---
+++ src/Spice/Routing/RouteMatch.php
@@ -0,0 +1,141 @@
+<?php
+
+class Spice_Routing_RouteMatch implements ArrayAccess {
+    /**
+     * @var string O nome da rota que corresponde à requisição atual.
+     */
+    private $routeName;
+
+    /**
+     * @var array Um array associativo contendo os parâmetros de rota encontrados.
+     */
+    private $params;
+
+    /**
+     * Inicializa um objeto `RouteMatch` com um nome e os parâmetros encontrados. 
+     *
+     * @param string $name O nome da rota correspondente.
+     * @param array<string,string> $params Os parâmetros de rota encontrados.
+     */
+    public function __construct($name, array $params = array()) {
+        $this->setRouteName($name);
+        $this->setParams($params);
+    }
+    
+    /**
+     * Estabelece o nome da rota que casa com a requisição atual.
+     *
+     * @param string $name O nome da rota.
+     *
+     * @return Spice_Routing_RouteMatch Fluent interface.
+     */
+    public function setRouteName($name) {
+        $this->routeName = (string) $name;
+        return $this;
+    }
+
+    /**
+     * Obtém o nome da rota que casa com a requisição atual.
+     *
+     * @return string
+     */
+    public function getRouteName() {
+        return $this->routeName;
+    }
+
+    /**
+     * Estabelece os parâmetros de rota.
+     *
+     * @param array<string,string> $params Os parâmetros.
+     *
+     * @return Spice_Routing_RouteMatch Fluent interface.
+     */
+    public function setParams(array $params) {
+        foreach ($params as $key => $value) {
+            $this->setParam($key, $value);
+        }
+        return $this;
+    }
+
+    /**
+     * Obtém os parâmetros de rota.
+     * 
+     * @return array<string,string>
+     */
+    public function getParams() {
+        return $this->params;
+    }
+
+    /**
+     * Estabelece um parâmetro de rota.
+     *
+     * @param string $name O nome do parâmetro.
+     * @param string $name O nome do parâmetro.
+     *
+     * @return Spice_Routing_RouteMatch Fluent interface.
+     */
+    public function setParam($name, $value) {
+        $this->params[(string) $name] = (string) $value;
+        return $this;
+    }
+
+    /**
+     * Remove um parâmetro de rota.
+     *
+     * @param string $name O nome do parâmetro.
+     * @param string $name O nome do parâmetro.
+     *
+     * @return Spice_Routing_RouteMatch Fluent interface.
+     */
+    public function unsetParam($name) {
+        if (isset($this->params[$name])) {
+            unset($this->params[$name]);
+        }
+        return $this;
+    }
+
+    /**
+     * Obtém um parâmetro da rota.
+     * Caso o parâmetro não exista, `$default` será retornado em seu lugar.
+     *
+     * @param string $name O nome do parâmetro. 
+     * @param mixed $default O valor padrão a se retornar caso o parâmetro não exista.
+     *
+     * @return mixed Se o parâmetro existe, o retorno será uma string, se não, o tipo
+     *      de retorno será o mesmo do parâmetro `$default`.
+     */
+    public function getParam($name, $default = null) {
+        if (isset($this->params[$name])) {
+            return $this->params[$name];
+        }
+        return $default;
+    }
+
+    /**
+     * {@inherit-doc}
+     */
+    public function offsetGet($offset) {
+        return $this->getParam($offset);
+    }
+
+    /**
+     * {@inherit-doc}
+     */
+    public function offsetSet($offset, $value) {
+        return $this->setParam($offset, $value);
+    }
+    
+    /**
+     * {@inherit-doc}
+     */
+    public function offsetUnset($offset) {
+        return $this->unsetParam($offset);
+    }
+
+    /**
+     * {@inherit-doc}
+     */
+    public function offsetExists($offset) {
+        return $this->getParam($offset) !== null;
+    }
+}
---
+++ tests/Spice/Routing/RouteMatchTest.php
@@ -0,0 +1,138 @@
+<?php
+class Spice_Routing_RouteMatchTest extends PHPUnit_Framework_TestCase {
+
+    private function createRouteMatch($routeName, array $params = array()) {
+        return new Spice_Routing_RouteMatch($routeName, $params);
+    }
+
+    /**
+     * @testdox Testa se é possível criar uma instância do objeto.
+     * @test
+     */
+    public function testCreateInstance() {
+        $match = $this->createRouteMatch('foo'); 
+        $this->assertInstanceOf('Spice_Routing_RouteMatch', $match);
+    }
+
+    /**
+     * @testdox Testa se os parâmetros da rota correspondente são corretamente armazenados.
+     * @test
+     */
+    public function testGetParams() {
+        $params = array('param1' => 'value1');
+        $match = $this->createRouteMatch('foo', $params);
+        $this->assertAttributeEquals($params, 'params', $match);
+        $this->assertEquals($params, $match->getParams());
+    }
+
+    /**
+     * @testdox Testa um parâmetro da rota correspondente está corretamente armazenado.
+     * @test
+     */
+    public function testGetExistentParam() {
+        $params = array('param1' => 'value1');
+        $match = $this->createRouteMatch('foo', $params);
+        $this->assertArrayHasKey('param1', $match->getParams());
+        $this->assertEquals($params['param1'], $match->getParam('param1'));
+        $this->assertEquals($params['param1'], $match['param1']);
+        $this->assertEquals($params['param1'], $match->getParam('param1', 'defaultValue'));
+    }
+
+    /**
+     * @testdox Testa se o retorno do método `getParam` será o mesmo que 
+     *      o passado como seu parâmetro `$default`.
+     * @test
+     */
+    public function testGetInexistentParam() {
+        $params = array('param1' => 'value1');
+        $default = 'value2';
+        $match = $this->createRouteMatch('foo', $params);
+
+        $this->assertNull($match->getParam('param2'));
+        $this->assertEquals($default, $match->getParam('param2', $default));
+    }
+
+    /**
+     * @testdox Testa o método `setParam` funciona adequadamente tanto para 
+     *      novos parâmetros quanto para existentes.
+     * @test
+     */
+    public function testSetParams() {
+        $paramName = 'param1';
+        $params = array($paramName => 'value1');
+        $match = $this->createRouteMatch('foo', $params);
+
+        $newParamValue = 'baz';
+
+        $match->setParam($paramName, $newParamValue);
+
+        $this->assertEquals($newParamValue, $match->getParam($paramName));
+        $this->assertEquals($newParamValue, $match->getParam($paramName, 'defaultValue'));
+
+        $newParamName = 'param2';
+        $newParamValue = 'value2';
+
+        $match[$newParamName] = $newParamValue;
+
+        $this->assertArrayHasKey($newParamName, $match->getParams());
+        $this->assertEquals($newParamValue, $match->getParam($newParamName));
+        $this->assertEquals($newParamValue, $match->getParam($newParamName, 'defaultValue'));
+    }
+
+    /**
+     * @testdox Testa a remoção de um parâmetro.
+     * @test
+     */
+    public function testUnsetParam() {
+        $paramName = 'param1';
+        $paramName2 = 'param2';
+        $params = array($paramName => 'value1', $paramName2 => 'value2');
+        $match = $this->createRouteMatch('foo', $params);
+
+        $match->unsetParam($paramName);
+        $this->assertArrayNotHasKey($paramName, $match->getParams());
+        $this->assertNull($match->getParam($paramName));
+
+        unset($match[$paramName2]);
+        $this->assertNull($match->getParam($paramName2));
+    }
+
+    /**
+     * @testdox Testa se um parâmetro existe.
+     * @test
+     */
+    public function testIssetParam() {
+        $paramName = 'param1';
+        $params = array($paramName => 'value1');
+        $match = $this->createRouteMatch('foo', $params);
+
+        $this->assertTrue(isset($match[$paramName]));
+    }
+
+    /**
+     * @testdox Testa se o nome da rota correspondente está correto.
+     * @test
+     */
+    public function testGetRouteName() {
+        $name = 'foo';
+        $match = $this->createRouteMatch($name);
+
+        $this->assertAttributeEquals($name, 'routeName', $match);
+        $this->assertEquals($name, $match->getRouteName());
+    }
+
+    /**
+     * @testdox Testa se altera-se corretamente o nome da rota correspondente.
+     * @test
+     */
+    public function testSetRouteName() {
+        $name = 'foo';
+        $match = $this->createRouteMatch($name);
+        $newName = 'bar';
+
+        $match->setRouteName($newName);
+
+        $this->assertAttributeEquals($newName, 'routeName', $match);
+        $this->assertEquals($newName, $match->getRouteName());
+    }
+}
