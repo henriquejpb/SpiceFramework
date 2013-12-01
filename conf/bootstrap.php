f6c5a4345e6e634c5b6e4a09f264392f052a8676
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-12-01 13:34:24 -0200

    Adds composer compatibility

--- conf/bootstrap.php
+++ conf/bootstrap.php
@@ -1,42 +1,17 @@
 <?php
 /**
  * @author Henrique Barcelos <rick.hjpbarcelos@gmail.com>
- * @copiright (c) 2013, Henrique Barcelos
+ * @copyright (c) 2013, Henrique Barcelos
  */
 
 ini_set('display_errors', true);
 error_reporting(E_ALL | E_STRICT);
 
-setlocale(LC_ALL, array('es_ES', 'es_ES.iso-8859-1', 'es_ES.utf-8', 'spanish'));
-ini_set('date.timezone', 'Europe/Madrid');
+setlocale(LC_ALL, array('pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese'));
+ini_set('date.timezone', 'America/Sao_Paulo');
 
 if(!defined('APP_ROOT')) {
     define('APP_ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 }
 
-set_include_path(
-    implode(PATH_SEPARATOR,
-        array_unique(
-            array_merge(
-                array(
-                    APP_ROOT . 'src', 
-                    APP_ROOT . 'tests/unit'
-                ),
-                explode(PATH_SEPARATOR, get_include_path())
-            )
-        )
-    )
-);
-
-spl_autoload_register(
-    function ($class) {
-        $file = sprintf(
-            "%s.php", 
-            str_replace('\\', DIRECTORY_SEPARATOR, $class)
-        );
-
-        if (($classPath = stream_resolve_include_path($file)) != false) {
-            require $classPath;
-        }
-    }
-, true);
+require APP_ROOT.'vendor/autoload.php';
